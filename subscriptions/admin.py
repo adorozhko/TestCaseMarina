d305ebb96d9e3308fa1a7651d3c4d61cf895578a
Anton Dorozhko <adorozhko@icloud.com>
2021-05-01 23:43:12 +0300

    feat: create models

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+from .models import Author, Book, User
+
+
+@register(User)
+class UserAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'subscription'
+    )
+
+
+@register(Author)
+class AuthorAdmin(ModelAdmin):
+    list_display = (
+        'name',
+        'surname',
+        'about'
+    )
+    filter_horizontal = (
+        'books',
+    )
+
+
+@register(Book)
+class BookAdmin(ModelAdmin):
+    list_display = (
+        'title',
+        'type',
+        'genre'
+    )
+    filter_horizontal = (
+        'authors',
+    )
--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,78 @@
-from django.db import models
+from datetime import date, timedelta
 
-# Create your models here.
+from django.db.models import (PROTECT, CharField, DateField, ForeignKey,
+                              ManyToManyField, Model, TextChoices, TextField)
+from django.utils.translation import gettext_lazy as _
+
+
+class User(Model):
+    name = CharField(max_length=50)
+    subscription = DateField(
+        default=date.today() + timedelta(weeks=2)
+    )
+
+    def __str__(self):
+        return self.name
+
+    def is_active(self):
+        return self.subscription >= date.today()
+
+    def update_subscription(self, days):
+        self.subscription += timedelta(days=days)
+        self.save()
+        return self.subscription
+
+
+class Author(Model):
+    name = CharField(max_length=50)
+    surname = CharField(max_length=50)
+    about = TextField(blank=True)
+    books = ManyToManyField(
+        'Book',
+        through='AuthorBook',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Book(Model):
+    class Type(TextChoices):
+        TEXT = 'TXT', _('Text')
+        AUDIO = 'AUDIO', _('Audio')
+
+    class Genre(TextChoices):
+        COMEDY = 'COMEDY', _('Comedy')
+        TRAGEDY = 'TRAGEDY', _('Tragedy')
+        DRAMA = 'DRAMA', _('Drama')
+        HORROR = 'HORROR', _('Horror')
+
+    title = CharField(max_length=50)
+    type = CharField(
+        max_length=50,
+        choices=Type.choices,
+        default=Type.TEXT
+    )
+    genre = CharField(
+        max_length=50,
+        choices=Genre.choices,
+        default=Genre.DRAMA
+    )
+    about = TextField(blank=True)
+    authors = ManyToManyField(
+        'Author',
+        through='AuthorBook',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class AuthorBook(Model):
+    author = ForeignKey(Author, on_delete=PROTECT)
+    book = ForeignKey(Book, on_delete=PROTECT)
+
+    class Meta:
+        auto_created = True
